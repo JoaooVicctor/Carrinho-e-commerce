7a2daa14bb96bbf783aad804214eadd359a4224d
joaovictor <jcsjoaovictor@gmail.com>
2025-12-26 20:01:08 -0300

    Adicionando e estilizando a página Home e Carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,6 +1,31 @@
 
 export function Cart() {
   return(
-    <h1>Pagina carrinho de compras</h1>
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+      <section className="flex items-center justify-between border-b-2 border-grey-300">
+        <img className="w-28" src="https://boutique.octup.fr/storage/2022/11/CF58749_01-1024x1024.jpeg" alt="" />
+
+        <strong>Preço: R$1.000</strong>
+
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            -
+          </button>
+          1
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            +
+          </button>
+
+        </div>
+
+        <strong className="float-right">
+          Subtotal: R$1.000
+        </strong>
+      </section>
+
+      <p className="font-bold mt-4">Total: R$1.000</p>
+    </div>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,26 @@
-
+import { BsCartPlus} from 'react-icons/bs'
 
 export function Home() {
   return(
-    <h1>Pagina home</h1>
+    <main className="w-full max-w-7xl px-4 mx-auto">
+      <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+
+      <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5'>
+        <section className="w-full">
+          <img className='w-full rounded-bl-lg max-h-70 mb-2' src="https://boutique.octup.fr/storage/2022/11/CF58749_01-1024x1024.jpeg" alt="" />
+          <p className='font-medium mt-1 mb-2'>Airpods Pro</p>
+
+          <div className='flex gap-3 items-center'>
+            <strong className='text-zinc-700/90'>
+              R$ 1000,00
+            </strong>
+            <button className='bg-zinc-900 p-1 rounded'>
+              <BsCartPlus size={22} color="#FFF" />
+            </button>
+          </div>
+
+        </section>
+      </div>
+    </main>
   )
 }
