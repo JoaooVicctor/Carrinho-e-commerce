aa4df04d87987946e4a92e9a8942fb31f98b2da8
joaovictor <jcsjoaovictor@gmail.com>
2025-12-26 21:08:30 -0300

    adicionando Api dos produtos na página

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,40 @@
+import {useEffect, useState} from 'react'  
 import { BsCartPlus} from 'react-icons/bs'
 
+import { api } from '../../services/api'
+
+interface ProductProps{
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 export function Home() {
+  const [products, setProducts] = useState<ProductProps[]>([])
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products")
+      setProducts(response.data)
+    }
+    getProducts()
+  }, []);
+
   return(
     <main className="w-full max-w-7xl px-4 mx-auto">
       <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
 
       <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5'>
-        <section className="w-full">
-          <img className='w-full rounded-bl-lg max-h-70 mb-2' src="https://boutique.octup.fr/storage/2022/11/CF58749_01-1024x1024.jpeg" alt="" />
-          <p className='font-medium mt-1 mb-2'>Airpods Pro</p>
+        {products.map( (product) => (
+          <section key={product.id} className="w-full">
+          <img className='w-full rounded-bl-lg max-h-72 mb-2' src={product.cover} alt={product.title} />
+          <p className='font-medium mt-1 mb-2'>{product.title}</p>
 
           <div className='flex gap-3 items-center'>
             <strong className='text-zinc-700/90'>
-              R$ 1000,00
+              {product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
             </strong>
             <button className='bg-zinc-900 p-1 rounded'>
               <BsCartPlus size={22} color="#FFF" />
@@ -20,6 +42,7 @@ export function Home() {
           </div>
 
         </section>
+        ))}
       </div>
     </main>
   )
